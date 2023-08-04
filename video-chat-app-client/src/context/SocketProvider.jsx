6817dc8efaef7d7b62a2c8c7dc37c01eb3729447
Rahul721999 <109303988+Rahul721999@users.noreply.github.com>
2023-08-04 22:32:53 +0530

    Rahul/impl websocket (#2)

---
+++ video-chat-app-client/src/context/SocketProvider.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useContext } from "react";
+
+const SocketContext = createContext(null);
+
+
+export const useSocket = () =>{
+    const socket = useContext(SocketContext);
+    return socket;
+};
+
+export const SocketProvider = (props) =>{
+    const socket = new WebSocket("ws://localhost:8080/ws");
+    socket.onopen = () =>{
+        console.log("connected");
+    }
+
+    socket.onclose = (event) => {
+        console.log('WebSocket connection closed:', event.code, event.reason);
+    };
+  
+    return (
+        <SocketContext.Provider value ={socket}>
+            {props.children}
+        </SocketContext.Provider>
+    );
+}
--- video-chat-app-client/src/screens/Lobby.jsx
+++ video-chat-app-client/src/screens/Lobby.jsx
@@ -1,8 +1,11 @@
 import {React, useCallback, useState} from 'react';
-const socket = new WebSocket('ws://localhost:8080/ws');
+import {useSocket} from "../context/SocketProvider";
+
+
 const LobbyScreen = () => {
     const [email, setEmail] = useState("");
     const [room, setRoom] = useState("");
+    const socket = useSocket();
 
     const handleEmailChange = (e) => {
         e.preventDefault();
@@ -14,28 +17,28 @@ const LobbyScreen = () => {
     }
     const handleFormSubmit = useCallback((e) => {
         e.preventDefault();
-        socket.send(email);
-        socket.onmessage = (e) =>{
-            console.log(e.data)
+
+        socket.send(JSON.stringify({email: email, room: room}));
+
+        socket.onmessage = (msg) => {
+            let rec_msg = JSON.parse(msg.data);
+            console.log('"%s" joined the room', rec_msg.email);
         }
-        console.log({email, room})
-    }, 
-    [email, room])
-    return (
-        <div>
-            <h1>Lobby</h1>
-            <form>
-                <label htmlFor='email'>Email:</label>
-                <input type='email' id="email"
-                    onChange={handleEmailChange}></input>
-                <label htmlFor='room'>Room No:</label>
-                <input type='text' id="room"
-                    onChange={handleRoomChange}></input>
-                <button onClick={handleFormSubmit}
-                    type='submit'>Join</button>
-            </form>
-        </div>
-    )
+
+    }, [email, room, socket]);
+    return (<div>
+        <h1>Lobby</h1>
+        <form>
+            <label htmlFor='email'>Email:</label>
+            <input type='email' id="email"
+                onChange={handleEmailChange}></input>
+            <label htmlFor='room'>Room No:</label>
+            <input type='text' id="room"
+                onChange={handleRoomChange}></input>
+            <button onClick={handleFormSubmit}
+                type='submit'>Join</button>
+        </form>
+    </div>)
 }
 
 export default LobbyScreen;
